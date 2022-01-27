d4a6b61d19b9c3dc53ddafa0c11b9a7ba9ae9c3f
Aleksander <olehsander.karonski@gmail.com>
2022-01-27 23:59:46 +0100

    Kuberneterization

--- client/frontend-app/src/App.tsx
+++ client/frontend-app/src/App.tsx
@@ -8,42 +8,29 @@ function App() {
   const [currentTime, setCurrentTime] = useState(0);
 
   useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
+    fetch('/api/time').then(res => res.json()).then(data => {
       setCurrentTime(data.time)
     })
   }, []);
 
   return (
-    <div>
-      <p>Rucham psa jak sra</p>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p>The current time is {currentTime}</p>
+      </header>
     </div>
-    // <Router>
-    //   <header>
-    //     <div>This is a multicontainer application</div>
-    //     <Link to="/">Home</Link>
-    //     <Link to="/other">Other page</Link>
-    //   </header>
-    //   <div>
-    //     <Route exact path="/" component={MainComponent}></Route>
-    //   </div>
-    // </Router>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //     <p>The current time is {currentTime}</p>
-    //   </header>
-    // </div>
   );
 }
 
